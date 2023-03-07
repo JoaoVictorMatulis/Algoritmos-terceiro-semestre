6ae3c18749b30bb2819ac818ff7890641025f853
JoaoVictorMatulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-03-07 09:20:04 -0300

    Update Cadastro.java

--- ED/aula4/cadastro/Cadastro.java
+++ ED/aula4/cadastro/Cadastro.java
@@ -10,20 +10,26 @@ public class Cadastro {
     Array.inserir(new Pessoa("Sandra", 30));
     Array.inserir(new Pessoa("Jorge", 30));
     Array.inserir(new Pessoa("Jarge", 40));
-    Array.inserir(new Pessoa("Jarge", 40));
-    Array.inserir(new Pessoa("Jarge", 40));
-    Array.inserir(new Pessoa("Jarge", 40));
-    Array.inserir(new Pessoa("Jarge", 40));
-    Array.inserir(new Pessoa("Jarge", 40));
+    Array.inserir(new Pessoa("Aarge", 40));
+    Array.inserir(new Pessoa("Darge", 40));
+    Array.inserir(new Pessoa("CAlarge", 40));
+    Array.inserir(new Pessoa("Awarge", 40));
+    Array.inserir(new Pessoa("Terarge", 40));
     Array.mostrar();
-    System.out.println("=== Pesquisando... ===");
-    int id = 187;
-    Pessoa p = Array.buscar(id);
-    if(p != null){
-      System.out.println(p);
+    System.out.println("\n=== Pesquisando ===\n");
+    int id = 9;
+    int p = Array.buscar(id);
+    if (p != -1) {
+      Array.mostrarId(id);
+    } else {
+      System.out.println("Não achei o id:" + id);
     }
-    else{
-      System.out.println("Não achei o "+id);
+    System.out.println("\n=== Removendo ===\n");
+    if (Array.remover(id)) {
+      Array.mostrar();
+      System.out.println("\nCadastro removido com sucesso.\n");
+    } else {
+      System.out.println("Não foi possível achar esse cadastro.");
     }
   }
 
