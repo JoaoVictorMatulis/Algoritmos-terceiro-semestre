e303b8dc02b865d46a5852b2ce6d0e016b8a3fb5
João Victor Matulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-05-03 18:06:22 -0300

    Delete POO/aulaBD directory

--- POO/aulaBD/bandodedadosPOO.sql
+++
@@ -1,7 +0,0 @@
-use mydb;
-create table aluno (
-   id int primary key auto_increment,
-   nome varchar(40) not null
-);
-insert into aluno (nome) VALUES("Gilson Nascimento"), ("Jonas Perreira"),("Thomas Souza"),("Leandro Filho"),("João Victor");
-select * from aluno;
