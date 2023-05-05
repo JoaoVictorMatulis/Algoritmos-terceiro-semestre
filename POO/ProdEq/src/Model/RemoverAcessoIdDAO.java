c7ffe2e74ffe75bc123dbbeabe1a9f13a789e75b
João Victor Matulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-05-04 23:30:57 -0300

    Add files via upload

---
+++ POO/ProdEq/src/Model/RemoverAcessoIdDAO.java
@@ -0,0 +1,27 @@
+/*
+ * Turma: A
+    Autores: João Victor Matulis || Id de aluno: 1142445416
+            Bernardo Galvão de Souza || Id de aluno: 1142473154
+            Gabriel de Melo Silva || Id de aluno: 1141267353
+            Kevin de Sousa dos Santos || Id de aluno: 1142168549
+
+    Professor: Carlos Veríssimo
+ */
+package Model;
+
+public class RemoverAcessoIdDAO {
+
+    public void remover(int id) {
+        try {
+            String query = "DELETE FROM registros WHERE fk_id_pessoa = " + id + ";";
+            ConectarBD.stm.executeUpdate(query);
+            query = "DELETE FROM pessoa WHERE id_pessoa = " + id + ";";
+            ConectarBD.stm.executeUpdate(query);
+            System.out.println("ID: "+ id + "EXCLUIDO COM SUCESSO");
+            
+        } catch(Exception e) {
+            System.out.println("Erro na Exclusao: "+ e.getMessage());
+        }
+    }
+    
+}
---
+++ POO/ProdEq/src/Model/VerificarMoradoresDAO.java
@@ -0,0 +1,58 @@
+/*
+ * Turma: A
+    Autores: João Victor Matulis || Id de aluno: 1142445416
+            Bernardo Galvão de Souza || Id de aluno: 1142473154
+            Gabriel de Melo Silva || Id de aluno: 1141267353
+            Kevin de Sousa dos Santos || Id de aluno: 1142168549
+
+    Professor: Carlos Veríssimo
+ */
+package Model;
+
+import java.sql.ResultSet;
+
+public class VerificarMoradoresDAO {
+    ResultSet result = null;
+
+    public String[][] listar() {
+        String[][] resultados = null;
+        try {
+            resultados = new String[tamanho()][8];
+            String minhaQuery = "SELECT * FROM pessoa ORDER BY nome_pessoa";
+            result = ConectarBD.stm.executeQuery(minhaQuery);
+            int linha = 0;
+            int coluna = 0;
+            while (result.next()) {
+                resultados[linha][coluna++] = result.getString("id_pessoa");
+                resultados[linha][coluna++] = result.getString("nome_pessoa");
+                resultados[linha][coluna++] = result.getString("ap");
+                resultados[linha][coluna++] = result.getString("bloco");
+                resultados[linha][coluna++] = result.getString("cpf");
+                resultados[linha][coluna++] = result.getString("fone");
+                resultados[linha][coluna++] = result.getString("email");
+                resultados[linha++][coluna] = result.getString("senha");
+                coluna = 0;
+            }
+            return resultados;
+        } catch (Exception e) {
+            System.out.println("Erro na lista: " + e.getMessage());
+        }
+        return resultados;
+    }
+
+    public int tamanho() {
+        int tamanho = 0;
+        try {
+            String minhaQuery = "SELECT COUNT(*) AS total FROM pessoa";
+            result = ConectarBD.stm.executeQuery(minhaQuery);
+            while (result.next()) {
+                System.out.println("Number of columns in the table : " + result.getInt("total"));
+                tamanho = result.getInt("total");
+            }
+            return tamanho;
+        } catch (Exception e) {
+            System.out.println("Erro na lista: " + e.getMessage());
+        }
+        return tamanho;
+    }
+}
