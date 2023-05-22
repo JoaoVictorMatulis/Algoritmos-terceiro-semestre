86395296abcf11177ce50b71a7e624d1eb71ada8
João Victor Matulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2023-05-22 10:51:31 -0300

    Add files via upload

---
+++ POO/SENAC-POOK483-Equipe6-GestaoMoradores-main/ProdEq/src/Controller/Iniciar.java
@@ -0,0 +1,20 @@
+/*
+ * Turma: A
+    Autores: João Victor Matulis || Id de aluno: 1142445416
+            Bernardo Galvão de Souza || Id de aluno: 1142473154
+            Gabriel de Melo Silva || Id de aluno: 1141267353
+            Kevin de Sousa dos Santos || Id de aluno: 1142168549
+
+    Professor: Carlos Veríssimo
+ */
+package Controller;
+
+import Model.ConectarBD;
+import View.Morador.SenhaView;
+
+public class Iniciar {
+    public Iniciar(){
+        new ConectarBD();
+        new SenhaView();
+    }
+}
